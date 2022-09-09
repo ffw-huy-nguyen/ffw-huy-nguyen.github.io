f35687a03192e463da87b64ec149b46ee4bbbe9b
Eddie <eddie@hectre.com>
2022-09-09 10:58:36 +0700

    Update

---
+++ _nuxt/static/1662695863/termsandconditions/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/termsandconditions", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1662695863/termsandconditions/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662695863",layout:"toc",error:a,state:{config:{}},serverRendered:true,routePath:"\u002Ftermsandconditions",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
